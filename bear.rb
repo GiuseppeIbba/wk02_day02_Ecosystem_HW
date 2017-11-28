a1b6d2cd9185575fdf9daa5541c90b89c7c8e768
Giuseppe <giuseppe.ibba@hotmail.com>
2017-11-28 18:03:23 +0000

    Added test_bear_has_a_name

--- bear.rb
+++ bear.rb
@@ -1,2 +1,16 @@
 class Bear
-end
+
+
+    attr_reader(:name)
+
+      def initialize(name)
+        @name = name
+        @stomach = []
+      end
+
+      def bear_has_a_name()
+         @bear.name()
+      end
+
+
+  end
--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -10,4 +10,8 @@ class TestBear < MiniTest::Test
     @bear = Bear.new("Yogi")
   end
 
+def test_bear_has_a_name()
+  assert_equal("Yogi", @bear.name())
+end
+
 end
